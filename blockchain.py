cb23e8158ab294f5a74a9b46d54a935770f5cb7c
sippinyotrippin <van.didenko@gmail.com>
2022-12-24 15:25:55 +0300

    Express our blockchain as class which got some methods

---
+++ blockchain.py
@@ -0,0 +1,17 @@
+class Blockchain:
+
+    def __init__(self):
+        self.chain = []
+
+    def new_block(self):
+    # Generates new block and adds to the chain
+        pass
+
+    @staticmethod
+    def hash(block):
+    # Hashes a block
+        pass
+
+    def last_block(self):
+    # Получает последний блок в проходе цепочки
+        pass
