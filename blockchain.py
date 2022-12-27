f812616539f89d50761bade170627a27b8c2b8ab
sippinyotrippin <van.didenko@gmail.com>
2022-12-27 12:28:47 +0300

    line 31: use sha256 hash-function to hash a block

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,6 @@
 import json
 from datetime import datetime
+from hashlib import sha256
 
 
 class Blockchain:
@@ -25,11 +26,13 @@ class Blockchain:
     @staticmethod
     def hash(block):
     # Hashes a block
-       block_string = json.dumps(block, sort_keys=True).encode()
+        block_string = json.dumps(block, sort_keys=True).encode()
+        # sort_keys=True means all keys of output dict will be sorted
+        return sha256(block_string).hexdigest()
 
     def last_block(self):
     # Returns last block of the chain
-       pass
+        pass
 
     def new_transaction(self, sender, recipient, amount):
         self.pending_transactions.append({
